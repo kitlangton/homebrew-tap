8630035ecdbbdefdbc9d440ec2d575fe1d56af92
Kit Langton <kit.langton@gmail.com>
2026-05-03 12:35:27 -0400

    Update ghui to 0.4.3

--- Formula/ghui.rb
+++ Formula/ghui.rb
@@ -1,28 +1,28 @@
 class Ghui < Formula
   desc "Terminal UI for GitHub pull requests"
   homepage "https://github.com/kitlangton/ghui"
-  version "0.4.2"
+  version "0.4.3"
   license "MIT"
 
   depends_on "gh"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-darwin-arm64.tar.gz"
-      sha256 "f797e96b765a15f214fb5a897df1a4d7145e984eba6d43e68cedd50d1d67c50b"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.3/ghui-darwin-arm64.tar.gz"
+      sha256 "92f9fc615997cb865937c19aae0c8f87d58430d3a2e5d3680a0a9658f2d3ffe2"
     else
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-darwin-x64.tar.gz"
-      sha256 "5b4b4ca6b89240a752b8afbc1ffa4a4ac3a7b8f81c0794fe8b184c20c3d03598"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.3/ghui-darwin-x64.tar.gz"
+      sha256 "8874ee2cf2acfb8df97ad4b1a4bb0d9b9220a4711a9e36b927388230fe0c9a6d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-linux-arm64.tar.gz"
-      sha256 "ae6f1bd6bd1ffd2643b9aa89d76987ccb80abcbeb2c4dce763d4a48e101d3e4d"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.3/ghui-linux-arm64.tar.gz"
+      sha256 "b60f75d24bedc44eb38999f13db5097b02089c74a99059420ef7bef24fda73ba"
     else
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-linux-x64.tar.gz"
-      sha256 "c1716846a4c6383420c4571fbf6ab3a4b4b58a48ea06af787d792e5679326401"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.3/ghui-linux-x64.tar.gz"
+      sha256 "a071f715ca479e37cfa03951a3adc1ad6bd578f9375b1a9d06fa8f00a173f7e6"
     end
   end
 
