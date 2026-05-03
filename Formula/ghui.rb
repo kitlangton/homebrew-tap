acb69dd0158a14091e6c60798d78ac93c573ca71
Kit Langton <kit.langton@gmail.com>
2026-05-03 11:08:35 -0400

    Update ghui to 0.4.2

--- Formula/ghui.rb
+++ Formula/ghui.rb
@@ -1,30 +1,30 @@
 class Ghui < Formula
   desc "Terminal UI for GitHub pull requests"
   homepage "https://github.com/kitlangton/ghui"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
+
+  depends_on "gh"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.1/ghui-darwin-arm64.tar.gz"
-      sha256 "515bd5245210563093b69c5e7e07eba94634812d34e73fa27a0e890ae741f3b6"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-darwin-arm64.tar.gz"
+      sha256 "f797e96b765a15f214fb5a897df1a4d7145e984eba6d43e68cedd50d1d67c50b"
     else
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.1/ghui-darwin-x64.tar.gz"
-      sha256 "2ba23d983c760677cfc277e3b126e1095ee9882eb4c10bf89130fb5cbf64f7b3"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-darwin-x64.tar.gz"
+      sha256 "5b4b4ca6b89240a752b8afbc1ffa4a4ac3a7b8f81c0794fe8b184c20c3d03598"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.1/ghui-linux-arm64.tar.gz"
-      sha256 "c72af300fdb75c6d8910ea3c01824d44558f1d6a4cb0b7f8bcc031f759c71d60"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-linux-arm64.tar.gz"
+      sha256 "ae6f1bd6bd1ffd2643b9aa89d76987ccb80abcbeb2c4dce763d4a48e101d3e4d"
     else
-      url "https://github.com/kitlangton/ghui/releases/download/v0.4.1/ghui-linux-x64.tar.gz"
-      sha256 "ba6743d67aa5bb62aae67cd3fb58fb55fea6b2aaae2765143c1e1b2b4f7bd82d"
+      url "https://github.com/kitlangton/ghui/releases/download/v0.4.2/ghui-linux-x64.tar.gz"
+      sha256 "c1716846a4c6383420c4571fbf6ab3a4b4b58a48ea06af787d792e5679326401"
     end
   end
-
-  depends_on "gh"
 
   def install
     bin.install "ghui"
